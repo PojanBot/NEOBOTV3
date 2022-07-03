b6ae9ce8e696ed3eee347e7157ea5602e63f91cc
PojanBot <102582676+PojanBot@users.noreply.github.com>
2022-07-03 19:45:21 +0700

    Update help.js

--- message/help.js
+++ message/help.js
@@ -10,9 +10,9 @@ return `
 exports.txtDonate = (prefix) => {
 return `
 ▷_*DONASI BOT*_
- _- DANA : 08xxxxxxxx_
+ _- DANA : 088270863279_
  _- OVO : 08xxxxxxxx_
- _- GOPAY : 08xxxxxxxx_
- _- QRIS : https://bit.ly/3yy0qoZ_
+ _- GOPAY : 088270863279_
+ _- QRIS : Belum Bikin_
 `
 }
